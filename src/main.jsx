f8dda9b8285ebf205ba58da269be4fcb60d0b174
Karachi O <karachilauren05@gmail.com>
2025-02-07 20:05:37 -0500

    Utilizing picocss framework

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import '@picocss/pico/css/pico.min.css';
+import './custom.css'; // Your styles will override PicoCSS
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
